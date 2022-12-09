c9192c0e731af92e76f538d9ac0c01cd60ea82a0
Raul Rosa Pacheco <rr0608@outlook.com>
2022-12-09 19:36:39 -0300

    Create Attribute-Transformation-and-Parsing.sql

---
+++ Attribute-Transformation-and-Parsing.sql
@@ -0,0 +1,20 @@
+# Adding a column(commission) of type decimal with attributes with 
+# 10 positions and 2 decimal places, after the column sale_value
+ALTER TABLE `data_cleansing`.`tb_sales`
+ADD COLUMN `commission`DECIMAL(10,2) NULL AFTER `sale_value`;
+
+SELECT * FROM data_cleansing.tb_sales;
+
+SET SQL_SAFE_UPDATES = 0;
+
+UPDATE data_cleansing.tb_sales
+SET commission = 5
+WHERE empID = 1;
+
+UPDATE data_cleansing.tb_sales
+SET commission = 6
+WHERE empID = 2;
+
+
+
+
