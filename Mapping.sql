e42e498c39f5c7703dd08e75a3ef3f04adcd6643
Raul Rosa Pacheco <rr0608@outlook.com>
2022-12-12 21:25:16 -0300

    Update Mapping.sql

--- Mapping.sql
+++ Mapping.sql
@@ -22,3 +22,30 @@ CREATE TABLE data_cleansing.tb_map_animal_zoo (
 
 INSERT INTO data_cleansing.tb_map_animal_zoo (id_animal, id_zoo)
 VALUES (1, 1001), (1, 1002), (2, 1001), (3, 1000), (4, 1001);
+
+SELECT * FROM data_cleansing.tb_animal;
+SELECT * FROM data_cleansing.tb_zoo;
+SELECT * FROM data_cleansing.tb_map_animal_zoo;
+
+SELECT A.name Animal, Z.name Zoo
+FROM data_cleansing.tb_animal A, data_cleansing.tb_zoo Z, data_cleansing.tb_map_animal_zoo M
+WHERE A.id = M.id_animal AND Z.id = M.id_zoo
+ORDER BY animal;
+
+SELECT A.name animal, Z.name zoo 
+FROM data_cleansing.tb_animal A, data_cleansing.tb_map_animal_zoo AS AtoZ, data_cleansing.tb_zoo Z
+WHERE AtoZ.id_animal = A.id AND Z.id = AtoZ.id_zoo 
+UNION
+SELECT B.name animal, Z.name zoo 
+FROM data_cleansing.tb_animal B, data_cleansing.tb_map_animal_zoo AS BtoZ, data_cleansing.tb_zoo Z
+WHERE BtoZ.id_animal = B.id AND Z.id = BtoZ.id_zoo
+ORDER BY animal;
+
+INSERT INTO data_cleansing.tb_animal (id, name)
+VALUES (5, 'Macaco');
+
+SELECT A.name AS animal, COALESCE(C.name, 'Sem Zoo') AS zoo
+FROM data_cleansing.tb_animal AS A 
+LEFT OUTER JOIN (data_cleansing.tb_map_animal_zoo AS B INNER JOIN data_cleansing.tb_zoo AS C ON C.id = B.id_zoo)
+ON B.id_animal = A.id
+ORDER BY animal;
