c6808a18d884d203eb912d675ceef8527bb86c3f
Raul Rosa Pacheco <rr0608@outlook.com>
2022-12-13 10:24:44 -0300

    Create Filtering-Aggregation-Summarization.sql

---
+++ Filtering-Aggregation-Summarization.sql
@@ -0,0 +1,93 @@
+# Filtering, summarizing and aggregation
+
+# Create table pipeline sales
+CREATE TABLE data_cleansing.tb_pipeline_sales (
+  `Account` text,
+  `Opportunity_ID` text,
+  `Sales_Agent` text,
+  `Deal_Stage` text,
+  `Product` text,
+  `Created_Date` text,
+  `Close_Date` text,
+  `Close_Value` text DEFAULT NULL
+);
+
+#  Load the dataset3.csv in the previous table by MySQL Workbench
+
+# Create table 
+CREATE TABLE data_cleansing.tb_sellers (
+  `Sales_Agent` text,
+  `Manager` text,
+  `Regional_Office` text,
+  `Status` text
+);
+
+# Load the dataset4.csv in the previous table by MySQL Workbench
+
+# Answer the items below with SQL Language
+
+# 1- Total sales
+SELECT COUNT(*) total_sales 
+FROM data_cleansing.tb_pipeline_sales;
+
+# 2- Total amount sold
+SELECT SUM(CAST(Close_Value AS UNSIGNED)) total_amount 
+FROM data_cleansing.tb_pipeline_sales;
+
+# 3- Number of sales completed successfully
+SELECT COUNT(*) total_sales 
+FROM data_cleansing.tb_pipeline_sales 
+WHERE Deal_Stage = 'Won';
+
+# 4- Average value of sales completed successfully
+SELECT AVG(CAST(Close_Value AS UNSIGNED)) avg_amount 
+FROM data_cleansing.tb_pipeline_sales
+WHERE Deal_Stage = 'Won';
+
+# 5- Maximum amounts sold
+SELECT MAX(CAST(Close_Value AS UNSIGNED)) max_amount 
+FROM data_cleansing.tb_pipeline_sales;
+
+# 6- Minimum amount sold among successfully completed sales
+SELECT MIN(CAST(Close_Value AS UNSIGNED)) min_amount 
+FROM data_cleansing.tb_pipeline_sales
+WHERE Close_Value > 0;
+
+# 7- Average value of successfully completed sales per sales agent
+SELECT Sales_Agent, AVG(CAST(Close_Value AS UNSIGNED)) avg_amount 
+FROM data_cleansing.tb_pipeline_sales
+WHERE Deal_Stage = 'Won'
+GROUP BY Sales_Agent;
+
+#8- Average value of successfully completed sales per sales agent manager
+SELECT S.Manager, AVG(CAST(P.Close_Value AS UNSIGNED)) avg_amount 
+FROM data_cleansing.tb_pipeline_sales P, data_cleansing.tb_sellers S
+WHERE S.Sales_Agent = P.Sales_Agent
+AND Deal_Stage = 'Won'
+GROUP BY S.Manager;
+
+# 9- Total closing value of the sale per sales agent and on account of successfully completed sales
+SELECT Sales_Agent, Account, SUM(CAST(Close_Value AS UNSIGNED)) sum_amount 
+FROM data_cleansing.tb_pipeline_sales
+WHERE Deal_Stage = 'Won'
+GROUP BY Sales_Agent, Account
+ORDER BY Sales_Agent;
+
+# 10- Number of sales per sales agent for successfully completed sales and sales value greater than 1000
+SELECT Sales_Agent, COUNT(CAST(Close_Value AS UNSIGNED)) num_sales 
+FROM data_cleansing.tb_pipeline_sales
+WHERE Deal_Stage = 'Won'
+AND Close_Value > 1000
+GROUP BY Sales_Agent;
+# 11- Number of sales and average sales value per sales agent
+SELECT Sales_Agent, COUNT(CAST(Close_Value AS UNSIGNED)) num_sales, AVG(CAST(Close_Value AS UNSIGNED)) avg_value  
+FROM data_cleansing.tb_pipeline_sales
+WHERE Deal_Stage = 'Won'
+GROUP BY Sales_Agent;
+
+#12- Which sales agents had more than 30 sales?
+SELECT Sales_Agent, COUNT(CAST(Close_Value AS UNSIGNED)) num_sales, AVG(CAST(Close_Value AS UNSIGNED)) avg_value  
+FROM data_cleansing.tb_pipeline_sales
+WHERE Deal_Stage = 'Won'
+GROUP BY Sales_Agent
+HAVING num_sales > 30;
